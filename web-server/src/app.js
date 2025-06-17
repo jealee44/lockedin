f3ab99952415e08cd34f3a0cc06feb9ea87f6224
Jea Hyun Lee <jealee44@gmail.com>
2025-06-17 15:07:14 -0400

    set up proper views directory

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,9 +3,37 @@ const express = require('express')
 
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Jea Lee'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Jea Lee'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is helpful text'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
