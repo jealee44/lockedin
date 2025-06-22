0a670e6c208d51e720c50d0e94ecb8ffeb7539cf
Jea Hyun Lee <jealee44@gmail.com>
2025-06-22 00:03:38 -0400

    added fetch api to get data from url

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,17 @@
 console.log('CLientside js file is loaded!')
+
+fetch('https://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+fetch('http://localhost:3000/weather?address=boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location, data.forecast)
+        }
+    })
+})
