e9e797d1c53009236ff2cb6a51ae16642ac0fa68
Jea Hyun Lee <jealee44@gmail.com>
2025-06-30 18:25:59 -0400

    Implement user/task CRUD with Mongoose, add password hashing, authentication helpers, and practice Express middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/task.js')
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  if (req.method === 'GET') {
+    res.send('GET requests disabled')
+  } else {
+     next()
+  }
+})
+
+app.use((req, res, next ) => {
+  res.status(503).send('Site is currently down. Check back soon!')
+})
+
 app.use(express.json());
 app.use(userRouter)
 app.use(taskRouter)
@@ -14,3 +26,5 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
+
+
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../models/user')
+const User = require('../models/user');
+const { useReducer } = require('react');
 
 router.post('/users', async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({user, token});
   } catch (error) {
     res.status(400).send(error);
   }
 });
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({user, token})
+    } catch(e) {
+        res.status(400).send()
+    }
+})
 
 router.get('/users', async (req, res) => {
   try {
@@ -47,7 +59,15 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+    const user = await User.findById(req.params.id)
+
+    updates.forEach((update) => {
+        user[update] = req.body[update]
+    })
+
+    await user.save()
+
+    // const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
 
     if (!user) {
       return res.status(404).send()
