6baa25510eadbcd7976842bcb18440110ede7bb7
Jea Hyun Lee <jealee44@gmail.com>
2025-07-12 15:17:17 -0400

    crud unit tests for users

---
+++ task-manager/tests/user.test.js
@@ -0,0 +1,77 @@
+const request = require('supertest')
+const app = require('../src/app')
+const User = require('../src/models/user')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+    _id: userOneId,
+    name: 'Kevin',
+    email: 'kevin@example.com',
+    password: '56what!!',
+    tokens: [{
+        token: jwt.sign({_id: userOneId }, process.env.JWT_SECRET)
+    }]
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+test('Should signup a new user', async () => {
+    await request(app).post('/users').send({
+        name: 'Jea',
+        email: 'jea@example.com',
+        password: 'Mypass111!'
+    }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app)
+    .post('/users/login')
+    .send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login nonexisting user', async () => {
+    await request(app)
+    .post('/users/login')
+    .send({
+        email: 'badUser',
+        password: 'pass'
+    }).expect(400)
+})
+
+test('Should get profile for user', async () => {
+    await request(app)
+    .get('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should not get profile for unauth user', async () => {
+    await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401)
+})
+
+test('Should delete account for user', async () => {
+    await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should not delete account for unauth user', async () => {
+    await request(app)
+    .delete('/users/me')
+    .send()
+    .expect(401)
+})
